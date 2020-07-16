62d3e524d8f9ef487c4a3f1ac55ab60677d3da79
Evan Relf <evan@evanrelf.com>
2020-07-15 23:21:23 -0700

    Clean up `configuration.nix`
    Also, port 22 is implied by enabling OpenSSH (and maybe also the
    DigitalOcean import)

--- configuration.nix
+++ configuration.nix
@@ -1,22 +1,23 @@
 { lib, modulesPath, pkgs, ... }:
 
 {
-  imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
-    (modulesPath + "/virtualisation/digital-ocean-config.nix")
-  ];
+  imports =
+    lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
+      (modulesPath + "/virtualisation/digital-ocean-config.nix")
+    ];
 
   environment.systemPackages = with pkgs; [
+    fd
     htop
     kakoune
     neovim
-    tmux
     ripgrep
-    fd
+    tmux
   ];
 
   networking.hostName = "minecwaft";
 
-  networking.firewall.allowedTCPPorts = [ 22 25565 ];
+  networking.firewall.allowedTCPPorts = [ 25565 ];
 
   services.openssh = {
     enable = true;
@@ -29,25 +30,28 @@
     volumes = [ "/var/minecwaft/data:/data" ];
     workdir = "/var/minecwaft";
     log-driver = "journald";
+
     environment = {
       EULA = "TRUE";
+
+      VERSION = "1.16.1";
       TYPE = "PAPER";
       PAPERBUILD = "91";
-      VERSION = "1.16.1";
+
       SERVER_NAME = "minecwaft";
       MOTD = "minecwaft";
-      MAX_PLAYERS = "10";
+
       MODE = "survival";
       DIFFICULTY = "normal";
+      MAX_PLAYERS = "10";
       SPAWN_PROTECTION = "0";
       OPS = "evanrelf";
+
       GUI = "FALSE";
       INIT_MEMORY = "1G";
       MAX_MEMORY = "3700M";
     };
   };
 
-  system.activationScripts.minecwaft = ''
-    mkdir -p /var/minecwaft/data
-  '';
+  system.activationScripts.minecwaft = "mkdir -p /var/minecwaft/data";
 }
