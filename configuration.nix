5440d053d58a5266b4749a0806c940cb29767146
Evan Relf <evan@evanrelf.com>
2020-07-15 18:32:04 -0700

    Add NixOS configuration

---
+++ configuration.nix
@@ -0,0 +1,39 @@
+{ modulesPath, lib, ... }:
+
+{
+  imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
+    (modulesPath + "/virtualisation/digital-ocean-config.nix")
+  ];
+
+  networking.hostName = "minecwaft";
+
+  networking.firewall.allowedTCPPorts = [ 22 25565 ];
+
+  docker-containers.minecwaft = {
+    image = "itzg/minecraft-server";
+    ports = [ "25565:25565" ];
+    volumes = [ "/var/minecwaft/data:/data" ];
+    workdir = "/var/minecwaft";
+    environment = {
+      EULA = "TRUE";
+      TYPE = "PAPER";
+      PAPERBUILD = "208";
+      VERSION = "1.15.2";
+      SERVER_NAME = "minecwaft";
+      MOTD = "minecwaft";
+      ICON = builtins.path { name = "minecwaft-icon"; path = ./icon.png; };
+      MAX_PLAYERS = "10";
+      MODE = "survival";
+      DIFFICULTY = "normal";
+      SPAWN_PROTECTION = "0";
+      OPS = "evanrelf";
+      GUI = "FALSE";
+      INIT_MEMORY = "1G";
+      MAX_MEMORY = "3700M";
+    };
+  };
+
+  system.activationScripts.minecwaft = ''
+    mkdir -p /var/minecwaft/data
+  '';
+}
