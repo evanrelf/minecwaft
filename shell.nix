43faf072369b7c727ba487676a682ec21a75b929
Evan Relf <evan@evanrelf.com>
2020-07-15 18:51:40 -0700

    Add preflight checklist to `shellHook`

--- shell.nix
+++ shell.nix
@@ -1,11 +1,33 @@
+{ pure ? false # Provide all required packages, for use with `nix-shell --pure`
+}:
+
 let
   pkgs = import ./nixpkgs.nix {};
 
+  lib = pkgs.lib;
+
 in
   pkgs.mkShell {
-    buildInputs = with pkgs; [
-      git
-      nix
-      terraform
-    ];
+    buildInputs = with pkgs;
+      [ terraform ] ++ lib.optional pure [ git nix ];
+
+    shellHook = ''
+      command -v git >/dev/null \
+        || echo "Warning: Couldn't find git in your PATH"
+
+      command -v nix-instantiate >/dev/null \
+        || echo "Warning: Couldn't find nix-instantiate in your PATH"
+
+      test -n "$DIGITALOCEAN_TOKEN" \
+        || test -n "$DIGITALOCEAN_ACCESS_TOKEN" \
+        || echo "Warning: Missing ambient DigitalOcean credentials"
+
+      test -f "$HOME/.aws/credentials" \
+        || test -f "$AWS_SHARED_CREDENTIALS_FILE" \
+        || test \( -n "$AWS_ACCESS_KEY_ID" -a -n "$AWS_SECRET_ACCESS_KEY" \) \
+        || echo "Warning: Missing ambient AWS credentials"
+
+      test -n "$SSH_AUTH_SOCK" \
+        || echo "Warning: SSH_AUTH_SOCK isn't set"
+    '';
   }
