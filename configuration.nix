f32d91199bb61cca65d8a4f5a0699e789c6a5d65
Evan Relf <evan@evanrelf.com>
2020-07-15 23:48:00 -0700

    Configure more NixOS and Minecraft stuff

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,10 @@
 
   networking.hostName = "minecwaft";
 
-  networking.firewall.allowedTCPPorts = [ 25565 ];
+  networking.firewall.allowedTCPPorts = [
+    25565 # Server
+    25575 # RCON
+  ];
 
   services.openssh = {
     enable = true;
@@ -26,32 +29,64 @@
 
   docker-containers.minecwaft = {
     image = "itzg/minecraft-server";
-    ports = [ "25565:25565" ];
-    volumes = [ "/var/minecwaft/data:/data" ];
+    ports = [
+      "25565:25565" # Server
+      "25575:25575" # RCON
+    ];
+    volumes = [
+      "/var/minecwaft/data:/data"
+      "/var/minecwaft/plugins:/plugins"
+    ];
     workdir = "/var/minecwaft";
     log-driver = "journald";
 
     environment = {
-      EULA = "TRUE";
-
+      # Server
       VERSION = "1.16.1";
       TYPE = "PAPER";
       PAPERBUILD = "91";
 
+      # Metadata
       SERVER_NAME = "minecwaft";
       MOTD = "minecwaft";
 
+      # Gameplay
       MODE = "survival";
       DIFFICULTY = "normal";
-      MAX_PLAYERS = "10";
       SPAWN_PROTECTION = "0";
-      OPS = "evanrelf";
+      # WORLD = "" # Can be a URL to ZIP file containing an archived world
 
-      GUI = "FALSE";
+      # Administration
+      OPS = "evanrelf"; # Names separated by commas, e.g. "foo,bar,baz"
+      MAX_PLAYERS = "10";
+      # WHITELIST = "" # Names separated by commas, e.g. "foo,bar,baz"
+      ENABLE_RCON = "true";
+      RCON_PASSORD = "banana";
+
+      # Performance
       INIT_MEMORY = "1G";
       MAX_MEMORY = "3700M";
+      USE_AIKAR_FLAGS = "true";
+      USE_LARGE_PAGES = "true";
+
+      # Miscellaneous
+      OVERRIDE_SERVER_PROPERTIES = "true";
+      EULA = "true";
+      TZ = "America/Los_Angeles";
     };
   };
 
-  system.activationScripts.minecwaft = "mkdir -p /var/minecwaft/data";
+  system.activationScripts.minecwaft = ''
+    mkdir -p /var/minecwaft/{data,plugins}
+  '';
+
+  services.atd.enable = true;
+
+  systemd.services.nightly-reboot = {
+    script = "${pkgs.systemd}/bin/reboot";
+    serviceConfig.type = "oneshot";
+    startAt = "04:00:00";
+  };
+
+  time.timeZone = "America/Los_Angeles";
 }
